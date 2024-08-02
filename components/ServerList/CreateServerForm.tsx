9251eeea0c3e5869cf9e490165f094914adf094e
GagDrag <ojassuniljoshi@gmail.com>
2024-08-02 19:12:58 +0530

    Add files via upload

---
+++ components/ServerList/CreateServerForm.tsx
@@ -0,0 +1,172 @@
+import { useDiscordContext } from '@/contexts/DiscordContext';
+import { UserObject } from '@/model/UserObject';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useChatContext } from 'stream-chat-react';
+import UserCard from './UserCard';
+import { useStreamVideoClient } from '@stream-io/video-react-sdk';
+
+type FormState = {
+  serverName: string;
+  serverImage: string;
+  users: UserObject[];
+};
+
+const CreateServerForm = () => {
+  // Check if we are shown
+  const params = useSearchParams();
+  const showCreateServerForm = params.get('createServer');
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const router = useRouter();
+
+  // Data
+  const { client } = useChatContext();
+  const videoClient = useStreamVideoClient();
+  const { createServer } = useDiscordContext();
+  const initialState: FormState = {
+    serverName: '',
+    serverImage: '',
+    users: [],
+  };
+
+  const [formData, setFormData] = useState<FormState>(initialState);
+  const [users, setUsers] = useState<UserObject[]>([]);
+
+  const loadUsers = useCallback(async () => {
+    const response = await client.queryUsers({});
+    const users: UserObject[] = response.users
+      .filter((user) => user.role !== 'admin')
+      .map((user) => {
+        return {
+          id: user.id,
+          name: user.name ?? user.id,
+          image: user.image as string,
+          online: user.online,
+          lastOnline: user.last_active,
+        };
+      });
+    if (users) setUsers(users);
+  }, [client]);
+
+  useEffect(() => {
+    if (showCreateServerForm && dialogRef.current) {
+      dialogRef.current.showModal();
+    } else {
+      dialogRef.current?.close();
+    }
+  }, [showCreateServerForm]);
+
+  useEffect(() => {
+    loadUsers();
+  }, [loadUsers]);
+
+  return (
+    <dialog
+      className='absolute py-16 px-20 z-10 space-y-8 rounded-xl'
+      ref={dialogRef}
+    >
+      <Link href='/' className='absolute right-8 top-8'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-8 h-8 text-gray-500 hover:text-black hover:font-bold'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+          />
+        </svg>
+      </Link>
+      <h2 className='text-3xl font-bold text-gray-600'>Create new server</h2>
+      <form action={createClicked} className='flex flex-col'>
+        <label className='labelTitle' htmlFor='serverName'>
+          Server Name
+        </label>
+        <input
+          type='text'
+          id='serverName'
+          name='serverName'
+          value={formData.serverName}
+          onChange={(e) =>
+            setFormData({ ...formData, serverName: e.target.value })
+          }
+          required
+        />
+        <label className='labelTitle' htmlFor='serverImage'>
+          Image URL
+        </label>
+        <input
+          type='text'
+          id='serverImage'
+          name='serverImage'
+          value={formData.serverImage}
+          onChange={(e) =>
+            setFormData({ ...formData, serverImage: e.target.value })
+          }
+          required
+        />
+        <h2 className='mb-2 labelTitle'>Add Users</h2>
+        <div className='max-h-64 overflow-y-scroll my-4'>
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className='flex items-center justify-start w-full space-x-6 my-2'
+            >
+              <input
+                type='checkbox'
+                id={user.id}
+                name={user.id}
+                className='w-4 h-4 mb-0'
+                onChange={(event) => userBoxChecked(event.target.checked, user)}
+              />
+              <UserCard user={user} />
+            </div>
+          ))}
+        </div>
+        <button
+          type='submit'
+          className='bg-discord rounded p-3 text-white font-bold uppercase'
+        >
+          Create
+        </button>
+      </form>
+    </dialog>
+  );
+
+  function userBoxChecked(checked: Boolean, user: UserObject) {
+    if (checked) {
+      setFormData({
+        ...formData,
+        users: [...formData.users, user],
+      });
+    } else {
+      setFormData({
+        ...formData,
+        users: formData.users.filter((u) => u.id !== user.id),
+      });
+    }
+  }
+
+  function createClicked() {
+    if (!videoClient) {
+      console.log('[CreateServerForm] Video client not available');
+      return;
+    }
+    createServer(
+      client,
+      videoClient,
+      formData.serverName,
+      formData.serverImage,
+      formData.users.map((user) => user.id)
+    );
+    setFormData(initialState);
+    router.replace('/');
+  }
+};
+export default CreateServerForm;
---
+++ contexts/DiscordContext.tsx
@@ -0,0 +1,235 @@
+'use client';
+
+import { DiscordServer } from '@/app/page';
+import { MemberRequest, StreamVideoClient } from '@stream-io/video-client';
+import { createContext, useCallback, useContext, useState } from 'react';
+import { Channel, ChannelFilters, StreamChat } from 'stream-chat';
+import { DefaultStreamChatGenerics } from 'stream-chat-react';
+import { v4 as uuid } from 'uuid';
+
+type DiscordState = {
+  server?: DiscordServer;
+  callId: string | undefined;
+  channelsByCategories: Map<string, Array<Channel<DefaultStreamChatGenerics>>>;
+  changeServer: (server: DiscordServer | undefined, client: StreamChat) => void;
+  createServer: (
+    client: StreamChat,
+    videoClient: StreamVideoClient,
+    name: string,
+    imageUrl: string,
+    userIds: string[]
+  ) => void;
+  createChannel: (
+    client: StreamChat,
+    name: string,
+    category: string,
+    userIds: string[]
+  ) => void;
+  createCall: (
+    client: StreamVideoClient,
+    serverName: string,
+    channelName: string,
+    userIds: string[]
+  ) => Promise<void>;
+  setCall: (callId: string | undefined) => void;
+};
+
+const initialValue: DiscordState = {
+  server: undefined,
+  callId: undefined,
+  channelsByCategories: new Map(),
+  changeServer: () => {},
+  createServer: () => {},
+  createChannel: () => {},
+  createCall: async () => {},
+  setCall: () => {},
+};
+
+const DiscordContext = createContext<DiscordState>(initialValue);
+
+export const DiscordContextProvider: any = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [myState, setMyState] = useState<DiscordState>(initialValue);
+
+  const changeServer = useCallback(
+    async (server: DiscordServer | undefined, client: StreamChat) => {
+      let filters: ChannelFilters = {
+        type: 'messaging',
+        members: { $in: [client.userID as string] },
+      };
+      if (!server) {
+        filters.member_count = 2;
+      }
+
+      console.log(
+        '[DiscordContext - loadServerList] Querying channels for ',
+        client.userID
+      );
+      const channels = await client.queryChannels(filters);
+      const channelsByCategories = new Map<
+        string,
+        Array<Channel<DefaultStreamChatGenerics>>
+      >();
+      if (server) {
+        const categories = new Set(
+          channels
+            .filter((channel) => {
+              return channel.data?.data?.server === server.name;
+            })
+            .map((channel) => {
+              return channel.data?.data?.category;
+            })
+        );
+
+        for (const category of Array.from(categories)) {
+          channelsByCategories.set(
+            category,
+            channels.filter((channel) => {
+              return (
+                channel.data?.data?.server === server.name &&
+                channel.data?.data?.category === category
+              );
+            })
+          );
+        }
+      } else {
+        channelsByCategories.set('Direct Messages', channels);
+      }
+      setMyState((myState) => {
+        return { ...myState, server, channelsByCategories };
+      });
+    },
+    [setMyState]
+  );
+
+  const createServer = useCallback(
+    async (
+      client: StreamChat,
+      videoClient: StreamVideoClient,
+      name: string,
+      imageUrl: string,
+      userIds: string[]
+    ) => {
+      const messagingChannel = client.channel('messaging', uuid(), {
+        name: 'Welcome',
+        members: userIds,
+        data: {
+          image: imageUrl,
+          server: name,
+          category: 'Text Channels',
+        },
+      });
+      const callId = uuid();
+      const audioCall = videoClient.call('default', callId);
+      const audioChannelMembers: MemberRequest[] = userIds.map((userId) => {
+        return {
+          user_id: userId,
+        };
+      });
+
+      try {
+        const response = await messagingChannel.create();
+        console.log('[DiscordContext - createServer] Response: ', response);
+        const createdAudioCall = await audioCall.create({
+          data: {
+            custom: {
+              serverName: name,
+              channelName: 'General Voice Channel',
+            },
+            members: audioChannelMembers,
+          },
+        });
+        changeServer({ name, image: imageUrl }, client);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    [changeServer]
+  );
+
+  const createChannel = useCallback(
+    async (
+      client: StreamChat,
+      name: string,
+      category: string,
+      userIds: string[]
+    ) => {
+      if (client.userID) {
+        const channel = client.channel('team', {
+          name: name,
+          members: userIds,
+          data: {
+            server: myState.server?.name,
+            category: category,
+          },
+        });
+        try {
+          const response = await channel.create();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    },
+    [myState.server?.name]
+  );
+
+  const createCall = useCallback(
+    async (
+      client: StreamVideoClient,
+      serverName: string,
+      channelName: string,
+      userIds: string[]
+    ) => {
+      const callId = uuid();
+      const audioCall = client.call('default', callId);
+      const audioChannelMembers: MemberRequest[] = userIds.map((userId) => {
+        return {
+          user_id: userId,
+        };
+      });
+      try {
+        const createdAudioCall = await audioCall.create({
+          data: {
+            custom: {
+              serverName: serverName,
+              callName: channelName,
+            },
+            members: audioChannelMembers,
+          },
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    []
+  );
+
+  const setCall = useCallback(
+    (callId: string | undefined) => {
+      setMyState((myState) => {
+        return { ...myState, callId };
+      });
+    },
+    [setMyState]
+  );
+
+  const store: DiscordState = {
+    server: myState.server,
+    callId: myState.callId,
+    channelsByCategories: myState.channelsByCategories,
+    changeServer: changeServer,
+    createServer: createServer,
+    createChannel: createChannel,
+    createCall: createCall,
+    setCall: setCall,
+  };
+
+  return (
+    <DiscordContext.Provider value={store}>{children}</DiscordContext.Provider>
+  );
+};
+
+export const useDiscordContext = () => useContext(DiscordContext);
